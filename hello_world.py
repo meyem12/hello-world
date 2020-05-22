1e9eb8df8014db31638cd35499a8f037343576b4
Matthew Meyer <65614519+meyem12@users.noreply.github.com>
2020-05-22 13:47:30 -0500

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -2,3 +2,5 @@
 name = "Matthew Meyer"
 age = 21
 print("My name is " + name + " and I am " + age + " years old.")
+print("I enjoy sports, especially hockey and baseball")
+print("I am from St. Louis Park, MN")
